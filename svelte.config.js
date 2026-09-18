d512b3595b85d00d3b0be5a15b8775f7304560b0
Sebastian Steiner <sebastian.steiner@tuta.io>
2026-09-18 21:49:48 +0200

    slightly hacky base path setting

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,15 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+        }),
         paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+            base: process.argv.includes('dev') ? '/this-is-another-more-important-path' : process.env.BASE_PATH,
         },
     },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
@@ -14,7 +14,16 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: true,
+			}),
+			paths: {
+				base: process.argv.includes('dev') ? '' : '/daily'
+			}
 		})
 	],
 	test: {
